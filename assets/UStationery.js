78b83d4ad154cf92b0a10cf232030318612be4fe
dyahajengp <dyahajengp@gmail.com>
2022-02-20 01:49:25 +0800

    Revisi HTML& CSS + Adding JS

--- assets/UStationery.js
+++ assets/UStationery.js
@@ -1,36 +1,55 @@
-// const keranjang = {
-//     stickynotes: 0,
-//     highlighter: 0,
-//     notebook: 0,
-//     organizer: 0,
-//     journalingset: 0,
-//     card: 0
-// }
+let field = document.querySelector(".container-product");
+let item = Array.from(field.children);
+let pilih = document.getElementById('pilih');
+let produk = [];
 
-// const pesanan = document.querySelector(".card")
-// for (let card of pesanan) {
-//     card.addEventListener('click', function(event) {
-//         const target = event.target;
-
-//         if(target.classList.contains('stickynotes')){
-//             alert("keranjang");
-//             keranjang.stickynotes=+1;
-//             return
-//         } else if (target.classList.contains('highlighter')){
-//             keranjang.highlighter=+1;
-//             return;
-//         }
-//     });
-// }
- 
-let pesanan = [];
-let total = document.querySelector(".keranjang");
-
-function hitungPesanan(val) {
-    let divObj = document.querySelector(".card");
-    divObj.value = val;
-    pesanan.push(parseInt(divObj.value));
-    console.log(pesanan);
-    let jumlah = pesanan.reduce((a,b) => a+b);
-    total.innerHTML = 'Total belanja Anda : $(jumlah)';
+for (let i of item){
+    const last = i.lastElementChild;
+    const x = last.textContent.trim();
+    const y = Number(x.substring(2));
+    i.setAttribute('data-harga', y);
+    produk.push(i);
 }
+
+pilih.onchange = urutValue;
+
+function urutValue(){
+    if (this.value === 'default'){
+        while(field.firstChild) {
+            field.removeChild(field.firstChild);
+        }
+        field.append(...produk);
+    } if (this.value === 'dariMurah'){
+        urutElemendariMurah(field, item);
+    } if (this.value === 'dariMahal'){
+        urutElemendariMahal(field, item);
+    }
+}
+
+function urutElemendariMurah(field, item){
+    let urutItem = item.sort((a,b) => {
+        const ax = a.getAttribute('data-harga');
+        const bx = b.getAttribute('data-harga');
+
+        return ax - bx;
+    });
+    while(field.firstChild) {
+        field.removeChild(field.firstChild);
+    }
+    field.append(...urutItem);
+}
+
+function urutElemendariMahal(field, item){
+    let urutItem = item.sort((a,b) => {
+        const ax = a.getAttribute('data-harga');
+        const bx = b.getAttribute('data-harga');
+
+        return bx - ax;
+    });
+    while(field.firstChild) {
+        field.removeChild(field.firstChild);
+    }
+    field.append(...urutItem);
+}
+
+
